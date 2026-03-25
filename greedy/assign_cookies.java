88d0162553b5f0e1444cd773acf26c461ce4717f
brijesh2004 <brskumar0102@gmail.com>
2026-03-25 21:06:26 +0530

    Assign Cookies

---
+++ greedy/assign_cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int n=g.length;
+        int m = s.length;
+        int i=0 , j=0;
+        int ans = 0;
+        while(i<n&&j<m){
+            if(s[j]>=g[i]){
+                ans++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return ans;
+    }
+}
