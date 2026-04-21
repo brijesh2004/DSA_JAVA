1342ebf1bbfdb863564198a7317864373d871147
brijesh2004 <brskumar0102@gmail.com>
2026-04-21 22:23:33 +0530

    Delete Node in a BST

---
+++ BST/DeleteNodeinaBST.java
@@ -0,0 +1,41 @@
+import javax.swing.tree.TreeNode;
+
+public class DeleteNodeinaBST {
+    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } 
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } 
+        else {
+            // Node found
+
+            // Case 1: No child
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // Case 2: One child
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            // Case 3: Two children
+            TreeNode successor = findMin(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
+        }
+        return root;
+    }
+
+// find left min value to replace
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
