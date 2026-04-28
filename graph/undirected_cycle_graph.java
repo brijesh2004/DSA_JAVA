bde933c600b7fcfa9ba3597bc750cf55f77e7b79
brijesh2004 <brskumar0102@gmail.com>
2026-04-28 22:33:22 +0530

    cyle detection in Undirected graph

--- graph/undirected_cycle_graph.java
+++ graph/undirected_cycle_graph.java
@@ -8,53 +8,70 @@ import java.util.Map;
 import java.util.Queue;
 
 public class undirected_cycle_graph {
-    private boolean isCyclePresent(int src, List<List<Integer>> adj, boolean[] visited) {
 
-    Queue<Integer> q = new LinkedList<>();
-    Map<Integer, Integer> parent = new HashMap<>();
+    private boolean isCyclePresentBFS(int src, List<List<Integer>> adj, boolean[] vis) {
 
-    q.add(src);
-    visited[src] = true;
-    parent.put(src, -1);
+        Map<Integer, Integer> parent = new HashMap<>();
+        Queue<Integer> q = new LinkedList<>();
 
-    while (!q.isEmpty()) {
-        int node = q.poll();
+        q.add(src);
+        parent.put(src, -1);
+        vis[src] = true;
 
-        for (int nei : adj.get(node)) {
-            if (visited[nei] && parent.get(node) != nei) {
+        while (!q.isEmpty()) {
+            int front = q.poll();
+
+            for (int nei : adj.get(front)) {
+                if (vis[nei] && nei != parent.get(front)) {
+                    return true;
+                } else if (!vis[nei]) {
+                    vis[nei] = true;
+                    q.add(nei);
+                    parent.put(nei, front);
+                }
+            }
+
+        }
+        return false;
+
+    }
+
+    private boolean isCyclePresentDFS(int src, List<List<Integer>> adj, boolean[] vis, int parent) {
+
+        vis[src] = true;
+
+        for (int nei : adj.get(src)) {
+            if (!vis[nei]) {
+                if (isCyclePresentDFS(nei, adj, vis, src))
+                    return true;
+            } else if (nei != parent)
                 return true;
-            }
-            if (!visited[nei]) {
-                visited[nei] = true;
-                parent.put(nei, node);
-                q.add(nei);
+        }
+        return false;
+
+    }
+
+    public boolean isCycle(int V, int[][] edges) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<>());
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (isCyclePresentDFS(i, adj, visited, -1))
+                    return true;
             }
         }
+
+        return false;
     }
-
-    return false;
-}
-
-public boolean isCycle(int V, int[][] edges) {
-
-    List<List<Integer>> adj = new ArrayList<>();
-    for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
-
-    for (int[] e : edges) {
-        int u = e[0];
-        int v = e[1];
-        adj.get(u).add(v);
-        adj.get(v).add(u);
-    }
-
-
-    boolean[] visited = new boolean[V];
-    for (int i = 0; i < V; i++) {
-        if (!visited[i]) {
-            if (isCyclePresent(i, adj, visited)) return true;
-        }
-    }
-
-    return false;
-}
 }
