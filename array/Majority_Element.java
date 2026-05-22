1e3bfce8d26e0d94af2628d1c3738599add3fa3e
brijesh2004 <brskumar0102@gmail.com>
2026-05-23 00:25:39 +0530

    majority element

--- array/Majority_Element.java
+++ array/Majority_Element.java
@@ -17,4 +17,23 @@ class Solution {
         }
         return val;
     }
+
+    public int majorityElement1(int[] nums) {
+        int ans = nums[0];
+        int cnt = 1;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==ans){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+            if(cnt<0){
+                ans= nums[i];
+                cnt=1;
+            }
+        }
+        return ans;
+    }
 }
