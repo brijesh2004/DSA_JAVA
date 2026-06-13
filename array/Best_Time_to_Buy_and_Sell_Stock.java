3993bdb5d045a2f87b90946580293af3d2ad3cc7
brijesh2004 <brskumar0102@gmail.com>
2026-06-14 00:29:51 +0530

    Best time

--- array/Best_Time_to_Buy_and_Sell_Stock.java
+++ array/Best_Time_to_Buy_and_Sell_Stock.java
@@ -9,6 +9,16 @@ public class Best_Time_to_Buy_and_Sell_Stock {
         return maxi;
     }
 
+    //  public int maxProfit1(int[] prices) {
+    //     int maxi = 0;
+    //     int mini = prices[0];
+    //     for(int i=1;i<prices.length;i++){
+    //         mini = Math.min(mini , prices[i]);
+    //         maxi = Math.max(maxi , prices[i]-mini);
+    //     }
+    //     return maxi;
+    // }
+
      public int maxProfit1(int[] prices) {
         int maxi = 0;
         int mini = prices[0];
