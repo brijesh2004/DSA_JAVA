7f777a85ea1d54ddffba36ffbb856cd1c2d6f785
brijesh2004 <brskumar0102@gmail.com>
2026-06-23 23:39:20 +0530

    Remove Nth Node from the List

---
+++ linkedList/Remove_Nth_Node_From_The_List.java
@@ -0,0 +1,31 @@
+public class Reverse_Linked_List_Two {
+   private int listLen(ListNode head){
+        int len=0;
+        while(head!=null){
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode ans = head;
+        int len = listLen(head);
+        int rm = len-n;
+        head = ans;
+        ListNode prev = null;
+        int curr = 0;
+        while(head!=null){
+            if(curr==rm){
+                if(prev==null){
+                    ans = head.next;
+                }else{
+                    prev.next = head.next;
+                }
+            }
+            prev = head;
+            head = head.next;
+            curr++;
+        }
+        return ans;
+    }
+}
