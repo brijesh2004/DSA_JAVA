d667e9db39f2ae1e9aa0f53a5c1be451fa1a92d7
brijesh2004 <brskumar0102@gmail.com>
2026-06-27 00:48:01 +0530

    Invert Binary Tree

--- binarytree/Invert_Binary_Tree.java
+++ binarytree/Invert_Binary_Tree.java
@@ -2,8 +2,6 @@ public class Invert_Binary_Tree{
     public void solve(TreeNode root){
         if(root==null) return ;
 
-        if(root.left==null && root.right==null) return;
-
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
