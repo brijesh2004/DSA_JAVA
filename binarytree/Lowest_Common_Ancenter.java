3cd3edb16e062794f10c2276aa0a00df9a4d1547
brijesh2004 <brskumar0102@gmail.com>
2026-07-22 00:12:45 +0530

    Lowest Common Ancestor

---
+++ binarytree/Lowest_Common_Ancenter.java
@@ -0,0 +1,16 @@
+class Lowest_Common_Ancestor {
+   private TreeNode solve(TreeNode root, TreeNode p, TreeNode q){
+        if(root==null) return null;
+        if(p==root || q==root) return root;
+
+        TreeNode left = solve(root.left , p , q);
+        TreeNode right = solve(root.right , p , q);
+
+        if(left!=null&&right!=null) return root;
+        return left!=null?left:right;
+
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return solve(root , p , q);
+    }
+}
